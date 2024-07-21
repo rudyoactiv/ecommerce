6156ad889d73c5f9bedb3e7e2390b008a83bf124
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-07-22 00:09:59 +0530

    add home page categories

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+import Card from "../Card/Card";
+
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "Long Sleeve Graphic T-Shirt",
+      isNew: true,
+      oldPrice: 19,
+      newPrice: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1972116/pexels-photo-1972116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      img2: "https://images.pexels.com/photos/1163198/pexels-photo-1163198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      newPrice: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      img2: "https://images.pexels.com/photos/994517/pexels-photo-994517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "Casual Denim Jacket",
+      isNew: false,
+      oldPrice: 35,
+      newPrice: 25,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      img2: "https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "Leather Boots",
+      isNew: true,
+      oldPrice: 80,
+      newPrice: 60,
+    },
+  ];
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map(item=>(
+            <Card item={item} key = {item.id}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import SLider from '../../components/Slider/Slider'
+import Slider from '../../components/Slider/Slider'
 import './Home.scss'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 
 const Home = () => {
   return (
     <div className="home">
-      <SLider />
+      <Slider />
+      <FeaturedProducts type="featured"/>
+      <Categories />
+      <FeaturedProducts type="trending"/>
+      <Contact />
     </div>
   )
 }
