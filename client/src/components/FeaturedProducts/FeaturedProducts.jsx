e430ad4fe9288496110f9258cb225aaa75838637
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-07-25 20:47:36 +0530

    pre filtering

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,49 +1,31 @@
 import React from "react";
-import "./FeaturedProducts.scss";
 import Card from "../Card/Card";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-
-
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(process.env.REACT_APP_API_URL + "/products?populate=*", {
-          headers: {
-            Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-          },
-        });
-        
-        setData(res.data.data)
-      } catch(err) {
-        console.log(err)
-      
-      }
-    }
-    fetchData();
-  }, []);
-
-  console.log(data)
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
   return (
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} products</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+          tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit.
         </p>
       </div>
       <div className="bottom">
-        {data.map(item=>(
-            <Card item={item} key = {item.id}/>
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
