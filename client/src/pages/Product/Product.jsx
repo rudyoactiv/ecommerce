75ab13486c8e756ccc73372014690837549edf8d
Rudraneel Dutta <its.rudraneel@gmail.com>
2024-07-26 00:23:40 +0530

    completed product page design

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -5,66 +5,93 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import favouriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanaceIcon from "@mui/icons-material/Balance";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(0);
 
-  const images = [
-    "https://images.pexels.com/photos/10026481/pexels-photo-10026481.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  ];
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>Title</h1>
-        <span className="price">$199</span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-        <div className="quantity">
-          <button onClick={() => setQuantity((prev) => prev === 0? 0 : prev - 1)}>-</button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AddShoppingCartIcon /> ADD TO CART
-        </button>
-        <div className="link">
-          <div className="item">
-            <FavoriteBorder /> ADD TO WISHLIST
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
           </div>
-          <div className="item">
-            <BalanaceIcon /> ADD TO COMPARE
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">${data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  setQuantity((prev) => (prev === 0 ? 0 : prev - 1))
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+              <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="link">
+              <div className="item">
+                <FavoriteBorder /> ADD TO WISHLIST
+              </div>
+              <div className="item">
+                <BalanaceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="details">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <span>Vendor: Polo</span>
-          <span>Product Type: T-Shirt</span>
-          <span>Tag: T-Shirt, Women, Top</span>
-        </div>
-        <hr />
-        <div className="details">
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
